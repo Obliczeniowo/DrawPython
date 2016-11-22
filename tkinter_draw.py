ff609a241a00ddccf83d738b252cf2657d10df44
Obliczeniowo <ZajaczkowskiKrzysztofKZ@gmail.com>
2016-11-22 18:38:29 +0100

    Change way of drawing objects and add some command line with history window

--- tkinter_draw.py
+++ tkinter_draw.py
@@ -62,6 +62,7 @@ class DrawingObject:
 	def __del__(self):
 		try:
 			self.object_tree.delete(str(self.id))
+			self.canvas.delete(self.id)
 		except:
 			pass
 	def __str__(self):
@@ -76,6 +77,13 @@ class DrawingObject:
 			str_config += "{name}=\"{value}\";".format(name = item[0], value = item[1][4])
 		str_config = str_config.strip(";")
 		return "type=\"{typename}\";coords=\"{coords}\";{config}".format(typename = self._typename, coords = str_coords, config = str_config)
+	def setCoords(self, coords):
+		try:
+			self.canvas.coords(self.id, tuple(coords))
+			return True
+		except:
+			return False
+	
 	@staticmethod
 	def GetCoords(dictionary):
 		coords = dictionary["coords"].strip(",").split(",")
@@ -113,7 +121,7 @@ class Rectangle(DrawingObject):
 		self.canvas.coords(self.id, min(x2, self.x), min(y2, self.y), max(x2, self.x), max(y2, self.y))
 	@staticmethod
 	def GetObject(dictionary, canvas, object_tree):
-		if dictionary["type"] == "Rectangle":
+		if dictionary["type"] in ["Rectangle", "rectangle", "Prostokąt", "prostokąt"]:
 			coords = DrawingObject.GetCoords(dictionary)
 			del(dictionary["coords"])
 			del(dictionary["type"])
@@ -155,7 +163,7 @@ class Ellipse(DrawingObject):
 		self.canvas.coords(self.id, center[0] - rays[0], center[1] - rays[1], center[0] + rays[0], center[1] + rays[1])
 	@staticmethod
 	def GetObject(dictionary, canvas, object_tree):
-		if dictionary["type"] == "Ellipse":
+		if dictionary["type"] in ["Ellipse", "ellipse", "Elipsa", "elipsa"]:
 			coords = DrawingObject.GetCoords(dictionary)
 			del(dictionary["coords"])
 			del(dictionary["type"])
@@ -185,10 +193,13 @@ class Line(DrawingObject):
 		self.canvas.coords(self.id, coords[0], coords[1], x2, y2)
 	@staticmethod
 	def GetObject(dictionary, canvas, object_tree):
-		if dictionary["type"] == "Line":
+		if dictionary["type"] in ["Line", "line", "Linia", "linia"]:
 			coords = DrawingObject.GetCoords(dictionary)
 			del(dictionary["coords"])
 			del(dictionary["type"])
+			if "outline" in dictionary:
+				dictionary["fill"] = dictionary["outline"]
+				del(dictionary["outline"])
 			return Line(coords, canvas, object_tree, **dictionary)
 		return None
 ################################################################################
@@ -217,7 +228,7 @@ class Polygon(DrawingObject):
 		coords += x2, y2
 		self.canvas.coords(self.id, tuple(coords))
 	def GetObject(dictionary, canvas, object_tree):
-		if dictionary["type"] == "Polygon":
+		if dictionary["type"] in ["Polygon", "polygon", "Wielokąt", "wielokąt"]:
 			coords = DrawingObject.GetCoords(dictionary)
 			del(dictionary["coords"])
 			del(dictionary["type"])
@@ -255,6 +266,8 @@ class Toolbar(tk.Frame):
 		if len(image):
 			self.images += [tk.PhotoImage(file = image)]
 			button.config(image = self.images[-1])
+	def set_on_buttons_change(self, fu):
+		self.valueset.trace("w", fu)
 	@property
 	def height(self):
 		return self.button_size
@@ -299,10 +312,10 @@ class Application:
 		########################################################################
 		
 		self.toolbar = Toolbar(self.window)
-		self.toolbar.add_button("L", image = "buttons/line.gif")
-		self.toolbar.add_button("O", image = "buttons/ellipse.gif")
-		self.toolbar.add_button("P", image = "buttons/rectangle.gif")
-		self.toolbar.add_button("Pol", image = "buttons/polygon.gif")
+		self.toolbar.add_button("Line", image = "buttons/line.gif")
+		self.toolbar.add_button("Ellipse", image = "buttons/ellipse.gif")
+		self.toolbar.add_button("Rectangle", image = "buttons/rectangle.gif")
+		self.toolbar.add_button("Polygon", image = "buttons/polygon.gif")
 		self.toolbar.add_button("M", image = "buttons/select.gif")
 		self.toolbar.place(x = self.toolbar.height * 4)
 		
@@ -324,7 +337,7 @@ class Application:
 		########################################################################
 		
 		self.canvas = tk.Canvas(self.window, background = "#ffffff", cursor = "tcross")
-		self.canvas.place(x = 250, y = self.toolbar.height, relwidth = 1., relheight = 1., height = - self.toolbar.height * 2, width = -250)
+		self.canvas.place(x = 250, y = self.toolbar.height, relwidth = 1., relheight = 1., height = - self.toolbar.height - 100, width = -250)
 		
 		self.object_tree = ttk.Treeview(self.window)
 		self.object_tree.place(x = 0, y = self.toolbar.height, width = 250, relheight = 1., height = - self.toolbar.height * 2)
@@ -340,39 +353,177 @@ class Application:
 		
 		self.selected = None
 		
+		self.lastpoint = [0,0]
+		self.addObject = False
+		
 		self.mousepointclicked = [0,0]
+		self.coords = []
 		
 		self.canvas.bind("<B1-Motion>", self.on_mousemoveb1)
 		self.canvas.bind("<Motion>", self.on_mousemove)
 		self.canvas.bind("<Button-1>", self.on_lbc)
 		self.canvas.bind("<ButtonRelease-1>", self.on_lbr)
 		self.canvas.bind("<Double-Button-1>", self.on_double_canvas_click)
+		self.canvas.bind("<Return>", self.on_return_canvas_click)
 		
 		self.canvas.bind("<Button-3>", self.on_rbc)
 		self.canvas.bind("<Delete>", self.on_delete)
+		
+		self.toolbar.set_on_buttons_change(self.on_toolbar_mode_changed)
+		
+		self.commandlinevar = tk.StringVar()
+		self.commandline = tk.Entry(self.window, textvariable = self.commandlinevar)
+		self.commandline.place(x = 250, y = - 50, rely = 1., in_ = self.window, relwidth = 1., width = - 250, height = 25)
+		self.commandline.bind("<Return>", self.on_return_commandline_click)
+		
+		self.commandhistory = tk.Text(self.window)
+		self.commandhistory.place(in_ = self.window, x = 250, y = - 100, rely = 1., width = -250, relwidth = 1., height = 50)
 		
 		self.drConstructors = [Rectangle.GetObject, Ellipse.GetObject, Line.GetObject, Polygon.GetObject]
 		
 		self.window.mainloop()
 	def on_mousemoveb1(self, event):
-		if self.toolbar.getvalueset == "P" or self.toolbar.getvalueset == "O" or self.toolbar.getvalueset == "L" or self.toolbar.getvalueset == "Pol":
-			self.draw_object[-1].setEnd(event.x, event.y)
-		elif self.toolbar.getvalueset == "M":
+		if self.toolbar.getvalueset == "M":
 			self.canvas.move(self.selected, event.x - self.mousepointclicked[0],event.y - self.mousepointclicked[1])
 			self.mousepointclicked = [event.x, event.y]
 	def on_mousemove(self, event):
 		self.mouse_position_on_canvas.set("x = {x}, y = {y}".format(x = event.x, y = event.y))
+		if self.addObject:
+			if self.toolbar.getvalueset == "Rectangle" or self.toolbar.getvalueset == "Ellipse" or self.toolbar.getvalueset == "Line" or self.toolbar.getvalueset == "Polygon":
+				self.draw_object[-1].setCoords(self.coords + [event.x, event.y])
+	def on_return_canvas_click(self, event):
+		self.addObject = False
+		if self.toolbar.getvalueset == "Rectangle" or self.toolbar.getvalueset == "Ellipse":
+			if len(self.coords) != 4:
+				del(self.draw_object[-1])
+			else:
+				self.draw_object[-1].setCoords(self.coords)
+			self.coords = []
+		elif self.toolbar.getvalueset == "Line" or self.toolbar.getvalueset == "Polygon":
+			if len(self.coords) < 4:
+				del(self.draw_object[-1])
+			else:
+				self.draw_object[-1].setCoords(self.coords)
+			self.coords = []
+	def on_return_commandline_click(self, event):
+		self.commandhistory.insert("end", "\n" + self.commandline.get() if self.commandhistory.get(1., "end") != "\n" else self.commandline.get())
+		self.commandhistory.see("end")
+		if self.addObject == False:
+			command = self.commandlinevar.get()
+			if len(command):
+				coords = command.split(" ")
+				if len(coords) > 0:
+					command = coords[0]
+					if len(coords) > 1 and len(coords) % 2 == 1:
+						coordst = coords[1:]
+						globalpt = True
+						if coordst[0].find("@") > -1:
+							print("global")
+							globalpt = False
+							coordst[0] = coordst[0][1:]
+						coords = ""
+						self.coords = []
+						i = 0
+						for coord in coordst:
+							if globalpt:
+								self.coords += [float(coord)]
+								coords += "," + coord
+								self.lastpoint[i % 2] = self.coords[-1]
+							else:
+								self.coords += [float(coord) + self.lastpoint[i % 2]]
+								coords += ",{0}".format(float(coord) + self.lastpoint[i % 2])
+								self.lastpoint[i % 2] = self.coords[-1]
+							i += 1
+					if len(self.coords) == 0:
+						coords = "{0},{1}".format(self.lastpoint[0], self.lastpoint[1])
+						coords += ",{0},{1}".format(self.lastpoint[0], self.lastpoint[1])
+					elif len(self.coords) == 2:
+						coords += ",{0},{1}".format(self.lastpoint[0], self.lastpoint[1])
+					print(coords)
+					dictionary = {"type": command, "coords": coords, "fill": self.fillcolor, "outline": self.strokecolor, "width": self.strokewidth.get()}
+					for constructor in self.drConstructors:
+						obj = constructor(dictionary, self.canvas, self.object_tree)
+						if obj != None:
+							self.draw_object += [obj]
+							self.commandlinevar.set("")
+							self.addObject = True
+							break;
+					if self.addObject == False:
+						self.coords = []
+						self.commandhistory.insert("end","\nPas zastawię czapke przedam, polecenie tego nie znam")
+		else:
+			coords = self.commandline.get().split(" ")
+			self.commandlinevar.set("")
+			if coords[0] == "c" and len(self.coords) > 1:
+				if len(self.coords) > 2:
+					self.lastpoint = self.coords[-4:-2]
+				else:
+					self.lastpoint = [0, 0]
+				self.coords = self.coords[:-2]
+				if len(self.coords) == 2:
+					self.draw_object[-1].setCoords(self.coords + self.lastpoint)
+				else:
+					self.addObject = self.draw_object[-1].setCoords(self.coords)
+			elif len(coords) > 1 and len(coords) % 2 == 0:
+				globalpt = True
+				if coords[0].find("@") > -1:
+					globalpt = False
+					coords[0] = coords[0][1:]
+				i = 0
+				for coord in coords:
+					if globalpt:
+						self.coords += [float(coord)]
+					else:
+						self.coords += [float(coord) + self.lastpoint[i % 2]]						
+					self.lastpoint[i % 2] = self.coords[-1]
+					i += 1
+				if len(self.coords) % 2 == 0:
+					if len(self.coords) == 2:
+						self.draw_object[-1].setCoords(self.coords + self.lastpoint)
+					else:
+						self.addObject = self.draw_object[-1].setCoords(self.coords)
+			else:
+				if len(self.coords) < 4:
+					del(self.draw_object[-1])
+				else:
+					self.draw_object[-1].setCoords(self.coords)
+				self.coords = []
+				self.addObject = False
+		
+	def on_toolbar_mode_changed(self, *args):
+		if self.addObject:
+			del(self.draw_object[-1])
+		self.addObject = False
+		self.coords = []
 	def on_lbc(self, event):
 		self.canvas.focus_set()
-		if self.toolbar.getvalueset == "P":
-			self.draw_object.extend([Rectangle([event.x, event.y, event.x, event.y], self.canvas, self.object_tree, fill = self.fillcolor, outline = self.strokecolor, width = self.strokewidth.get())])
-		elif self.toolbar.getvalueset == "O":
-			self.draw_object.extend([Ellipse([event.x, event.y, event.x, event.y], self.canvas, self.object_tree, fill = self.fillcolor, outline = self.strokecolor, width = self.strokewidth.get())])
-		elif self.toolbar.getvalueset == "L":
-			self.draw_object.extend([Line([event.x, event.y, event.x, event.y], self.canvas, self.object_tree, fill = self.strokecolor, width = self.strokewidth.get())])
-		elif self.toolbar.getvalueset == "Pol":
-			self.draw_object.extend([Polygon([event.x, event.y], self.canvas, self.object_tree, fill = self.fillcolor, outline = self.strokecolor, width = self.strokewidth.get())])
-		elif self.toolbar.getvalueset == "M":
+		
+		if not self.addObject:
+			if self.toolbar.getvalueset == "M":
+				self.addObject = False
+			else:
+				self.lastpoint = [event.x, event.y]
+			self.coords = [event.x, event.y]
+			if self.toolbar.getvalueset == "Rectangle":
+				self.commandlinevar.set("Rectangle {0} {1}".format(event.x, event.y))
+				self.on_return_commandline_click(event)
+			elif self.toolbar.getvalueset == "Ellipse":
+				self.commandlinevar.set("Ellipse {0} {1}".format(event.x, event.y))
+				self.on_return_commandline_click(event)
+			elif self.toolbar.getvalueset == "Line":
+				self.commandlinevar.set("Line {0} {1}".format(event.x, event.y))
+				self.on_return_commandline_click(event)
+			elif self.toolbar.getvalueset == "Polygon":
+				self.commandlinevar.set("Polygon {0} {1}".format(event.x, event.y))
+				self.on_return_commandline_click(event)
+		else:
+			if self.toolbar.getvalueset != "M":
+				print("{0} {1}".format(event.x, event.y))
+				self.commandlinevar.set("{0} {1}".format(event.x, event.y))
+				self.on_return_commandline_click(event)
+				#self.lastpoint = [event.x, event.y]
+		
+		if self.toolbar.getvalueset == "M":
 			self.selected = self.canvas.find_closest(event.x, event.y)
 			if self.selected:
 				config = self.canvas.itemconfig(self.selected)
@@ -423,6 +574,8 @@ class Application:
 			self.canvas.itemconfig(self.selected, outline = fc)
 	def on_rbc(self, events):
 		if len(self.draw_object):
+			self.addObject = False
+			self.coords = []
 			print(str(self.draw_object[-1]))
 			self.draw_object[-1].remove()
 			del(self.draw_object[-1])
